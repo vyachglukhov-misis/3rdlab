2fbcdf977f3c116ee0bbf21d6b2f570bb1078940
vyachglukhov-misis <m2210359@edu.misis.ru>
2022-12-16 17:19:04 +0300

    first commit

---
+++ lastLab/Program.cs
@@ -0,0 +1,20 @@
+﻿using MyStack;
+namespace Lab3
+{
+    class Lab
+    {
+        public static void Main(string[] args)
+        {
+            MyStack<string> stack = new MyStack<string>();
+            stack.Push("tom");
+            stack.Push("Alice");
+            stack.Push("ivan");
+
+            Console.WriteLine(stack.Head.Next.Data);
+            foreach(var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
